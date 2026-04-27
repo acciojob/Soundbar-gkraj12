4adbc8d9da502d0e74f35ad99c7a96015b057159
gaurav kumar <gauravkumarskg851215@gmail.com>
2026-04-28 01:31:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+let currentAudio = null;
+
+document.querySelectorAll(".btn").forEach((button, index) => {
+  button.addEventListener("click", function () {
+    if (currentAudio) {
+      currentAudio.pause();
+      currentAudio.currentTime = 0;
+    }
+
+    currentAudio = new Audio(`sounds/${sounds[index]}.mp3`);
+    currentAudio.play();
+  });
+});
+
+document.querySelector(".stop").addEventListener("click", function () {
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+  }
+});
